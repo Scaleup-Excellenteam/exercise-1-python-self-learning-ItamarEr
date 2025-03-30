8912de55b1cb733f6b65d1dc082d71ce0e3275fc
Itamar Erez <itamarrerez@gmail.com>
2025-03-30 16:18:16 +0300

    added parsle tongue

---
+++ src/5.3.parsle_tongue.py
@@ -0,0 +1,40 @@
+
+
+def parsle_tongue(filename):
+    """
+    This function reads a file called "logo.jpg" and extracts the hidden message from it.
+    The message is sequence of 5 or more lowercase english letters in a row separated by !.
+    """
+    read_size = 1024
+    message_min_size = 5
+
+    try:
+        with open(filename, 'rb') as file:  # Open the file in binary mode
+            while True:
+                data = file.read(read_size)       # Read the file in chunks of 1024 bytes
+                if not data:
+                    break
+                message = ""
+                index = 0
+                for char in data:
+                    char = chr(char)
+                    index += 1
+                    if char.isalpha() and char.islower() and char.isascii():
+                        message += char       # Append the character if it is a lowercase english letter.
+
+                    else:
+                        if char == "!" and len(message) >= message_min_size:
+                            # If the character is !, yield the message if it is at least 5 characters long.
+                            yield message
+                            print(index)
+                        message = ""
+
+    except FileNotFoundError:
+        print("File not found")
+
+
+if __name__ == "__main__":
+    filename = "logo.jpg"
+    gen = parsle_tongue(filename)
+    for word in gen:
+        print(word)
