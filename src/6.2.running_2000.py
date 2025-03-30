03abe66df8f08d9142e76cf05530cbb3ffb68b7a
Itamar Erez <itamarrerez@gmail.com>
2025-03-30 16:19:12 +0300

    added running 2000

---
+++ src/6.2.running_2000.py
@@ -0,0 +1,23 @@
+import time
+
+
+def running_2000(f,*args):
+    """
+    :param f: a function.
+    :param args: 0 or more arguments to pass to f.
+    :return: the time it took to run f.
+    """
+    start = time.time()
+    try:
+        f(*args)
+    except Exception as e:    # catch all exceptions: ValueError, TypeError, etc.
+        print(f"Error: {e}")
+        exit(1)
+    end = time.time()
+    return end - start
+
+
+
+if __name__ == '__main__':
+    print(running_2000(len, [1, 2, 3], [4, 5, 6]))
+    
