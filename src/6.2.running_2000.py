5a3251928b8a9536a55f51dc1f4ea9c9ecd0a58f
Itamar Erez <itamarere@m.telhai.ac.il>
2025-03-30 19:20:55 +0300

    Update 6.2.running_2000.py

--- src/6.2.running_2000.py
+++ src/6.2.running_2000.py
@@ -1,15 +1,16 @@
 import time
 
 
-def running_2000(f,*args):
+def running_2000(f,*args, **kwargs) -> float:
     """
     :param f: a function.
     :param args: 0 or more arguments to pass to f.
+    :param kwargs: 0 or more keyword arguments to pass to f.
     :return: the time it took to run f.
     """
     start = time.time()
     try:
-        f(*args)
+        f(*args, **kwargs)
     except Exception as e:    # catch all exceptions: ValueError, TypeError, etc.
         print(f"Error: {e}")
         exit(1)
