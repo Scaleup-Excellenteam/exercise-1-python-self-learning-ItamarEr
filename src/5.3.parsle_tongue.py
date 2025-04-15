57e31edbc2735471499cb7dbfcbeb46d9323ba07
Itamar Erez <itamarere@m.telhai.ac.il>
2025-04-15 14:46:40 +0300

    Fixed 5.3.parsle_tongue.py

--- src/5.3.parsle_tongue.py
+++ src/5.3.parsle_tongue.py
@@ -1,8 +1,5 @@
-"""
-This module provides a function to extract hidden messages from a binary file.
-The hidden message consists of sequences of at least 5 lowercase English letters,
-separated by the character '!'.
-"""
+READ_SIZE = 1024
+MESSAGE_MIN_SIZE = 5
 
 
 def parsle_tongue():
@@ -11,35 +8,32 @@ def parsle_tongue():
     The message is sequence of 5 or more lowercase english letters in a row separated by !.
     """
     filename = "logo.jpg"
-    read_size = 1024
-    message_min_size = 5
 
-    try:
-        with open(filename, 'rb') as file:  # Open the file in binary mode
-            while True:
-                data = file.read(read_size)       # Read the file in chunks of 1024 bytes
-                if not data:
-                    break
-                message = ""
-                index = 0
-                for char in data:
-                    char = chr(char)
-                    index += 1
-                    if char.isalpha() and char.islower() and char.isascii():
-                        message += char       # Append the character if it is a lowercase english letter.
+    with open(filename, 'rb') as file:  # Open the file in binary mode
+        while True:
+            data = file.read(READ_SIZE)       # Read the file in chunks of 1024 bytes
+            if not data:
+                break
+            message = ""
+            index = 0
+            for char in data:
+                char = chr(char)
+                index += 1
+                if char.isalpha() and char.islower() and char.isascii():
+                    message += char       # Append the character if it is a lowercase english letter.
 
-                    else:
-                        if char == "!" and len(message) >= message_min_size:
-                            # If the character is !, yield the message if it is at least 5 characters long.
-                            yield message
-                            print(index)
-                        message = ""
-
-    except FileNotFoundError:
-        print("File not found")
+                else:
+                    if char == "!" and len(message) >= MESSAGE_MIN_SIZE:
+                        # If the character is !, yield the message if it is at least 5 characters long.
+                        yield message
+                    message = ""
 
 
-if __name__ == "__main__":
+def main():
     gen = parsle_tongue()
     for word in gen:
         print(word)
+
+
+if __name__ == "__main__":
+    main()
